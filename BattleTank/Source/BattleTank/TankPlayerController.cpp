8f9f0cf5a568b06fd0bb63fb174984c11e163695
Raptagon Studios <raptagonstudios@gmail.com>
2018-07-17 10:48:14 -0600

    Created a c++ AIController and PlayerController which logs the name of the tank pawn it is posessing.

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not Possesing a tank."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller Possesing: %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+
+
