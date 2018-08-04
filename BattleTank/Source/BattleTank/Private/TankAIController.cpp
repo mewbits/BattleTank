f73e20ceede4bbc74797adcc88fa801853a2f1a6
Raptagon Studios <raptagonstudios@gmail.com>
2018-08-04 17:55:04 -0600

    Shrug

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "public/Tank.h"
 #include "TankAIController.h"
+#include "public/Tank.h"
 
 void ATankAIController::BeginPlay()
 {
